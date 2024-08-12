a66332ceb9a2718faf86964dc28ff70bbc3e7baa
szgk <szgkvr123@gmail.com>
2024-08-12 17:51:28 +0900

    faet: create get_anime_face_rate_AFD

--- src/get_cropped_anime_face.py
+++ src/get_cropped_anime_face.py
@@ -24,19 +24,13 @@ def get_cropped_anime_face():
         cv2_img = cv2.imread(image_path)
         _height, _width = cv2_img.shape[:2]
 
-        print(cv2_img)
-
         faces = face_cascade.detectMultiScale(cv2_img, 1.1, 3)
-
-        print(faces)
 
         for i, (x, y, w, h) in enumerate(faces):
             mv_mat = numpy.float32([[1, 0, -x + (w * face_area_ratio)], [0, 1, -y + (h * face_area_ratio)]])
             afin_image = cv2.warpAffine(cv2_img, mv_mat, (_width, _height))
             afin_image = afin_image[0: int((h / face_area_ratio) * (output_height / output_width)), 0: int(w / face_area_ratio)]
             afin_image = cv2.resize(afin_image, dsize = (output_width, output_height))
-
-            print(output_path + p_file.stem + str(i) + exe)
 
             cv2.imwrite(output_path + p_file.stem + str(i) + '.' + exe, afin_image)
 
