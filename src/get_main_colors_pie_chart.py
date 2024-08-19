42be9b4d73622b01993651856fab9e104bb98303
szgk <szgkvr123@gmail.com>
2024-08-20 04:12:24 +0900

    fix

--- src/get_main_colors_pie_chart.py
+++ src/get_main_colors_pie_chart.py
@@ -11,7 +11,7 @@ def get_main_colors_pie_chart():
 
     chart_data1 = data.get_color_name_chart_data(main_color_name_dict)
     chart_data2 = data.get_color_chart_data(main_color_name_dict)
-
-    chart.get_double_pie_chart_width_label(chart_data1, chart_data2, output_path)
+    print(main_color_name_dict)
+    chart.get_double_pie_chart(chart_data1, chart_data2, output_path)
 
 get_main_colors_pie_chart()
--- src/utils/regex.py
+++ src/utils/regex.py
@@ -2,5 +2,6 @@ import re
 
 def is_file_path(target):
     print(target)
-    print(re.match('.*\.[a-zA-Z]*$', target))
+    result = re.match('.*\.[a-zA-Z]*$', target)
+    print('is file path' if result else 'is not file path')
     return re.match('.*\.[a-zA-Z]*$', target)
